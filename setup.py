32a7f999b8e546bd29a7e3cb37ddf4ddb577cca9
daifdaifdaif <davidnaegeli@gmail.com>
2019-06-05 12:58:57 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+import os
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + '/requirements.txt'
+install_requires = [] 
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(name='sickbotsocial',
+      version='0.1',
+      description='Jessica Jurassica Bot',
+      long_description=long_description,
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+      keywords='textgeneration jessicajurassica dieyungenhurendothiv markov',
+      url='https://github.com/daifdaifdaif/sickbotsocial',
+      author='DAIF',
+      author_email='daif@dieyungenhuren.hiv',
+      license='MIT',
+      packages=['sickbotsocial'],
+      install_requires=install_requires,
+      include_package_data=True,
+      zip_safe=False)
