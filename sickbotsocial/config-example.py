cb81457d9713e19ed2a98f9879e629093c5063f2
daifdaifdaif <davidnaegeli@gmail.com>
2019-07-09 04:50:25 +0000

    large scale cleanup

--- sickbotsocial/config-example.py
+++ sickbotsocial/config-example.py
@@ -1,7 +1,7 @@
 # GLOBALS
-send_tweets = True  # whether to send tweets to API
-check_tweets = True  # whether to check tweets by JJ
-send_to_insta = True  # whether to post on insta
+send_tweets = False  # whether to send tweets to API
+check_tweets = False  # whether to check tweets by JJ
+send_to_insta = False  # whether to post on insta
 
 #############
 ### PATHS ###
